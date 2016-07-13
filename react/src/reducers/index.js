a0a2d933bf5ebf7a3650066a256ac691fb027f88
Jarryd Goodman <jarryd999@gmail.com>
2016-07-13 15:59:23 -0700

    import all actionTypes, refactor into filter & item reducer functions, created initial state objects for each function

--- react/src/reducers/index.js
+++ react/src/reducers/index.js
@@ -1,29 +1,52 @@
-import { ACTIVATE, SET_FILTER } from '../actionTypes';
+import { ACTIVATE, SET_FILTER, REQUEST_FILTERS, RECEIVE_FILTERS, REQUEST_ITEMS, RECEIVE_ITEMS } from '../actionTypes';
 
-const INITIAL = [
-    {id: 1, title: "Foo", source: "home"},
-    {id: 2, title: "Foo 2", source: "away"},
-    {id: 3, title: "Foo 3", source: "home"}
-];
+const INITIAL_FILTERS = {
+    isFetching: false,
+    activeFilter: null,
+    filters: []
+}
+const INITIAL_ITEMS = {
+    isFetching: false,
+    items: []
+};
 
-export function items(state = INITIAL, action) {
+
+
+export function filterState(state = INITIAL_FILTERS, action) {
+    switch (action.type) {
+        case SET_FILTER:
+            return Object.assign({}, state, { activeFilter: action.data });
+        case REQUEST_FILTERS:
+            return Object.assign({}, state, {isFetching : true});
+
+        case RECEIVE_FILTERS:
+            return Object.assign({}, state,{
+                isFetching : false,
+                filters: action.filters
+            })
+        default:
+            return state
+    }
+}
+
+export function itemState(state = INITIAL_ITEMS, action) {
     switch (action.type) {
         case ACTIVATE:
             let item = action.data;
-            return state.map(i => {
+            let updatedItems = state.itemState.items.map(i => {
                 i.active = (i.id == item.id);
                 return i;
             });
+            return Object.assign({}, state, {items: updatedItems});
+        case REQUEST_ITEMS:
+            return Object.assign({}, state, {isFetching: true});
+        case RECEIVE_ITEMS:
+            return Object.assign({}, state, {
+                isFetching: false,
+                items: action.items
+            });
         default:
-            return state;
+            return state
     }
 }
 
-export function activeFilter(state = null, action) {
-    switch (action.type) {
-        case SET_FILTER:
-            return action.data;
-        default:
-            return state;
-    }
-}
