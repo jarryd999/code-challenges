cb7643546ced83cc2fc0da7e4f595b068cdebafa
Jarryd Goodman <jarryd999@gmail.com>
2016-07-13 15:58:05 -0700

    imported thunk and reqs to make async fetch call, fetch filters before rendering app

--- react/src/index.js
+++ react/src/index.js
@@ -1,17 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { createStore, combineReducers } from 'redux';
+import { createStore, combineReducers, applyMiddleware } from 'redux';
+import thunkMiddleware from 'redux-thunk'
 import { Provider } from 'react-redux';
 
 import * as reducers from './reducers';
 import { App } from './components';
 import { FavoriteItemList, AvailableFilters } from './containers';
+import { fetchFilters } from './actions'
 
 import './index.scss';
 
 const store = createStore(
-    combineReducers(reducers)
+    combineReducers(reducers),
+    applyMiddleware(thunkMiddleware)
 );
+
+console.log(store.getState());
+store.dispatch(fetchFilters());
+console.log(store.getState());
 
 ReactDOM.render(
     <Provider store={store}>
