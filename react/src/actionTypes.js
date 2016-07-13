6b02dd4644e2358c570d6e86ec527ae635922c36
Jarryd Goodman <jarryd999@gmail.com>
2016-07-13 13:54:20 -0700

    added new action types for fetching filters/items

--- react/src/actionTypes.js
+++ react/src/actionTypes.js
@@ -1,2 +1,6 @@
 export const ACTIVATE = 'ACTIVATE';
 export const SET_FILTER = 'SET_FILTER';
+export const REQUEST_FILTERS = 'REQUEST_FILTERS';
+export const RECEIVE_FILTERS = 'RECEIVE_FILTERS';
+export const REQUEST_ITEMS = 'REQUEST_ITEMS';
+export const RECEIVE_ITEMS = 'RECEIVE_ITEMS';
