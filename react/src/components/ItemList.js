13c47b6f4ff1e64ed1e79303ce37144f78a25ea1
Jarryd Goodman <jarryd999@gmail.com>
2016-07-13 16:51:59 -0700

    updated what is props are passed down into Item

--- react/src/components/ItemList.js
+++ react/src/components/ItemList.js
@@ -5,7 +5,10 @@ export default function ItemList({items, onItemClick}) {
     return (
         <ul>
             {items.map(item => <Item
-                key={item.id} {...item}
+                key={item.id} 
+                source={item.source.display_name}
+                title={item.title}
+                active={item.active}
                 onClick={e => onItemClick(item)}
             />)}
         </ul>
